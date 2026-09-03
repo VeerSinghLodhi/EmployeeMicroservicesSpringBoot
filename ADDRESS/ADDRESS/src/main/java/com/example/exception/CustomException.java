cca7ec9174b9764c7a6d80b068342dec0b09d7d8
VeerSinghLodhi <veerlodhi54@gmail.com>
2026-09-03 23:29:50 +0530

    Using feign client service check whether employee exists or not.

---
+++ ADDRESS/ADDRESS/src/main/java/com/example/exception/CustomException.java
@@ -0,0 +1,25 @@
+package com.example.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomException extends RuntimeException{
+
+    HttpStatus status;
+
+    public CustomException(String message,HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+
+    public CustomException(String message){
+        super(message);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+}
